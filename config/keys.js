72efd0b28af1a4b82a83908f9e6017273d4b27d7
chitra <chitra.atmakuru@gmail.com>
2020-02-21 09:17:35 -0800

    change keys

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,5 @@
 module.exports = {
-  mongoURI: 'mongodb://localhost/PollDB',
+  //mongoURI: 'mongodb://localhost/PollDB',
+  mongoURI: 'mongodb://chitra:chitra19@ds055564.mlab.com:55564/pollreactapp',
+  secretOrKey: 'secret'
 }
